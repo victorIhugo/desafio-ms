907832e4a390732acd4eea7a47561794514eaad4
Victor Hugo <101189100+zVitao@users.noreply.github.com>
2023-07-31 02:38:07 -0300

    feat: importa publishobject module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PublishObjectModule } from './modules/publish-object/publish_object.module';
 
 @Module({
-  imports: [],
+  imports: [PublishObjectModule],
   controllers: [AppController],
   providers: [AppService],
 })
