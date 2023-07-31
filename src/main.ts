c61352bd6ff24fd4a1a2e3813037eba9d8beb6b7
Victor Hugo <101189100+zVitao@users.noreply.github.com>
2023-07-31 02:37:53 -0300

    feat: add middleware global
    feat: inicializa variaveis no dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { configDotenv } from 'dotenv';
 import { AppModule } from './app.module';
+import { logger } from './middleware/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(logger);
   await app.listen(3000);
+
+  configDotenv();
+
+  console.log('Application running on port 3000!');
 }
 bootstrap();
