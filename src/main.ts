511d29a04228a89f15b1dce0c7fe46b4e846cfbb
Victor Hugo <101189100+zVitao@users.noreply.github.com>
2023-07-31 23:27:13 -0300

    feat: add connect mongodb

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,38 @@
 import { NestFactory } from '@nestjs/core';
 import { configDotenv } from 'dotenv';
+import { MongoClient } from 'mongodb';
 import { AppModule } from './app.module';
-import { logger } from './middleware/logger.middleware';
+import { logger } from './common/middleware/logger.middleware';
+import { ConsumeService } from './modules/consume/consume.service';
+
+configDotenv();
+
+async function connectMongoDB() {
+  const mongoClient = new MongoClient(process.env.MONGODBCONNECT);
+  try {
+    await mongoClient.connect();
+
+    console.log('Connected to MongoDB!');
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await mongoClient.close();
+  }
+}
+connectMongoDB();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(logger);
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 
-  configDotenv();
-
-  console.log('Application running on port 3000!');
+  console.log(`Application running on port ${process.env.PORT}!`);
 }
 bootstrap();
+
+async function startConsumer() {
+  const consumer = new ConsumeService().consumer();
+
+  await consumer;
+}
+startConsumer();
