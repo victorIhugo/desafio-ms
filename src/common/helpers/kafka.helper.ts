3e8d490ca9c7a2bde6be2c990fa3ae5676b9738e
Victor Hugo <101189100+zVitao@users.noreply.github.com>
2023-08-01 21:10:12 -0300

    feat: add loglevel ao inicializar o kafka

--- src/common/helpers/kafka.helper.ts
+++ src/common/helpers/kafka.helper.ts
@@ -1,9 +1,10 @@
-import { Kafka } from 'kafkajs';
+import { Kafka, logLevel } from 'kafkajs';
 
 export function kafka(): Kafka {
   const kafka = new Kafka({
     clientId: 'my-app',
     brokers: [process.env.KAFKABROKER],
+    logLevel: logLevel.INFO,
   });
 
   return kafka;
