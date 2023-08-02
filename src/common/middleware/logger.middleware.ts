17588d8b7d967d26a3417d21fe25b1e2042de150
Victor Hugo <101189100+zVitao@users.noreply.github.com>
2023-08-01 21:10:22 -0300

    feat: add mais informações no logger

--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 
 export function logger(req: Request, res: Response, next: NextFunction) {
+  console.log(`${req.method} - ${req.headers.host}${decodeURI(req.url)} - ${res.statusCode}`);
   next();
 }
